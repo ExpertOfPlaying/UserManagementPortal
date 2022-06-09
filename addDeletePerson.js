a984c4f6309b5bc37901a8ec45846c267ed2fa6c
unknown <s0572782@htw-berlin.de>
2022-06-09 16:03:29 +0200

    update date

--- addDeletePerson.js
+++ addDeletePerson.js
@@ -4,6 +4,27 @@ function openAndCloseAddMenu() {
         document.getElementById("ocaddmenu").value = "Close Menu"
         document.getElementById("update").style.visibility = "hidden";
         document.getElementById("delete").style.visibility = "hidden";
+
+        let dateNow = new Date();
+        let dateToday = dateNow.getDate();
+        if(dateToday < 10){
+            dateToday = "0" + dateToday;
+        }
+        let monthToday = dateNow.getMonth() + 1;
+        //let monthMax = monthToday
+        if(monthToday < 10){
+            monthToday = "0" + monthToday;
+        }
+        let yearToday = dateNow.getUTCFullYear();
+        let yearMax = yearToday - 60;
+        let yearMin = yearToday - 17;
+        let yearJoin = 2015;
+        let dateMax = yearMin + "-" + monthToday + "-" + dateToday;
+        let dateMin = yearMax + "-" + monthToday + "-" + dateToday;
+        console.log(dateMax);
+        document.getElementById("DOB").setAttribute("max", dateMax);
+        document.getElementById("DOB").setAttribute("min", dateMin);
+
     }
 
     else {
@@ -22,8 +43,9 @@ function addUser() {
     const gender = document.querySelector('input[name="gender"]:checked').value;
     const dob = document.getElementById("DOB").value;
     const department = document.getElementById("departments").value;
+    const jd = document.getElementById("JD").value;
 
-    document.getElementById("userlist").innerHTML += `<tr><td>${Id}</td><td>${firstname}</td><td>${lastname}</td><td>${email}</td><td>${gender}</td><td>${dob}</td><td>${department}</td>
+    document.getElementById("userlist").innerHTML += `<tr><td>${Id}</td><td>${firstname}</td><td>${lastname}</td><td>${email}</td><td>${gender}</td><td>${dob}</td><td>${department}</td><td>${jd}</td>
         <td><input type="button" value="Delete"  class="delInner" style="visibility: hidden;" onclick="this.parentElement.parentElement.outerHTML = ''"/></td></tr>`;
 }
 
